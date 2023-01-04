8050badaaf4846067438239ba472e08c58397202
tomasda7 <tomasda7@outlook.com>
2023-01-04 14:29:21 -0300

    Final commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+
+        persona.setEdad(22);
+        persona.setNombre("Miguel Angel");
+        persona.setTelefono("344489809");
+
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Telefono: " + persona.getTelefono());
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+}
